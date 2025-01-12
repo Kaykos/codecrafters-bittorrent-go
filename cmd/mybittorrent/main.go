566f34bece9a8e28d670b2c4387d4077005f9d13
Sebastián Bobadilla <sebastian@zoefin.com>
2025-01-12 12:36:33 -0500

    Decode dictionaries

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -17,15 +17,27 @@ var _ = json.Marshal
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 func decodeBencode(bencodedString string) (interface{}, int, error) {
-	if isBencodedString(bencodedString) {
-		return decodeString(bencodedString)
-	} else if isBencodedInteger(bencodedString) {
+	switch bencodedString[0] {
+	case 'i':
 		return decodeInteger(bencodedString)
-	} else if isBencodedList(bencodedString) {
+	case 'l':
 		return decodeList(bencodedString)
-	} else {
-		return "", 0, fmt.Errorf("Only strings are supported at the moment")
+	case 'd':
+		return decodeDictionary(bencodedString)
+	default:
+		return decodeString(bencodedString)
 	}
+	//if isBencodedString(bencodedString) {
+	//	return decodeString(bencodedString)
+	//} else if isBencodedInteger(bencodedString) {
+	//	return decodeInteger(bencodedString)
+	//} else if isBencodedList(bencodedString) {
+	//	return decodeList(bencodedString)
+	//} else if isBencodedDictionary(bencodedString) {
+	//return decodeDictionary(bencodedString)
+	//} else {
+	//	return "", 0, fmt.Errorf("Only strings are supported at the moment")
+	//}
 }
 
 // Check if the first position of the string is a numerical digit
@@ -33,20 +45,22 @@ func isBencodedString(bencodedString string) bool {
 	return unicode.IsDigit(rune(bencodedString[0]))
 }
 
-// Check if the first position of the string is an 'i'
 func isBencodedInteger(bencodedString string) bool {
 	return bencodedString[0] == 'i'
 }
 
-// Check if the first position of the string is an 'l'
 func isBencodedList(bencodedString string) bool {
 	return bencodedString[0] == 'l'
+}
+
+func isBencodedDictionary(bencodedString string) bool {
+	return bencodedString[0] == 'd'
 }
 
 // Lists come as "l<bencoded_elements>e"
 func decodeList(bencodedString string) (interface{}, int, error) {
 	// Remove initial 'l'
-	elementsStr := bencodedString[1:]
+	elementsStr := bencodedString[1:] // e
 	// Slice of decoded elements
 	elements := []any{}
 	// Processed bytes for the whole list string
@@ -74,6 +88,47 @@ func decodeList(bencodedString string) (interface{}, int, error) {
 	return elements, processed + 2, nil
 }
 
+// Dictionries come as "d<key1><value1>...<keyN><valueN>e"
+func decodeDictionary(bencodedString string) (interface{}, int, error) {
+	// Remove initial 'd'
+	elementsStr := bencodedString[1:]
+	// Map of decoded elements
+	elements := map[string]any{}
+	// Processed bytes for the whole dictionary string
+	processed := 0
+	for {
+		// Found the end of the dictionary
+		if elementsStr[0] == 'e' {
+			break
+		}
+
+		// Decode single element
+		key, count, err := decodeString(elementsStr)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		// Move the initial position by the amount of processed bytes of the element
+		elementsStr = elementsStr[count:]
+		processed += count
+
+		// Decode single element
+		val, count, err := decodeBencode(elementsStr)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		// Move the initial position by the amount of processed bytes of the element
+		elementsStr = elementsStr[count:]
+		processed += count
+
+		elements[key] = val
+	}
+
+	// +2 to account for the 'd' and 'e'
+	return elements, processed + 2, nil
+}
+
 // Strings come as "10:strawberry"
 func decodeString(bencodedString string) (string, int, error) {
 	firstColonIndex := strings.IndexByte(bencodedString, ':')
@@ -92,6 +147,7 @@ func decodeString(bencodedString string) (string, int, error) {
 		nil
 }
 
+// i52e
 func decodeInteger(bencodedString string) (int, int, error) {
 	firstEIndex := strings.IndexByte(bencodedString, 'e')
 
@@ -106,6 +162,7 @@ func decodeInteger(bencodedString string) (int, int, error) {
 		return 0, 0, err
 	}
 
+	// +2 to account for 'i' and 'e'
 	return intVal, len(intStr) + 2, nil
 }
 
@@ -115,7 +172,7 @@ func main() {
 
 	if command == "decode" {
 		bencodedValue := os.Args[2]
-		//bencodedValue := "l10:strawberryi39ee"
+		//bencodedValue := "d3:foo3:bar5:helloi52ee"
 
 		decoded, _, err := decodeBencode(bencodedValue)
 		if err != nil {
